7633f798e40f9bccfc245a98d632360a24962115
ortoniKC <koushik350@gmail.com>
2022-11-08 13:05:36 +0530

    📦 NEW: Data Types

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # javascript-tamil-tutorial-2023
+
+Videos:
+
+[1. Node & VS Code Setup | JavaScript Tamil Tutorial - Part 1](https://youtu.be/2NVuia61QbE)
+[2. Introduction & Data Types | JavaScript Tamil Tutorial - Part 2](https://youtu.be/DbTMdEJPS2Q)
---
+++ basic/datatype.js
@@ -0,0 +1,15 @@
+// let, const, var
+
+let age = 27;
+console.log(typeof age);
+age = "twenty seven";
+console.log(typeof age);
+console.log(age);
+
+let a = 10;
+let b = "10";
+console.log(a + b);
+
+let isTutorialGood = true;
+isTutorialGood = "not good";
+console.log(isTutorialGood);
