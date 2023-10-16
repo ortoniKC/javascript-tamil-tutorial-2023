c326d81304c8c0f4d96772d6cc0c05db5864ac14
ortoniKC <koushik350@gmail.com>
2023-10-16 23:05:36 +0530

    📦 NEW: JS == vs ===

---
+++ stringConcepts/doubleVSTripleEquals.js
@@ -0,0 +1,14 @@
+let a = "apple";
+let b = new String("apple");
+// console.log(a === b);
+
+
+let num1 = 1;
+let num2 = "1";
+console.log(num1 === num2);
+
+
+let c = 10;
+let d = "11";
+
+console.log(c + Number(d));
